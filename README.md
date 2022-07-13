9855de24c6156f2da9ac05f4a0e98ef660b04288
Liz Washington <101292397+lizwashington@users.noreply.github.com>
2022-07-13 13:59:43 -0500

    Add files via upload
    PUSH

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# SQL
+# Employee-Tracker
+## The Coding Bootcamp at UT: Week 12 - SQL Challenge.
+
+# Grading Requirements
+This Challenge is graded based on the following criteria:
+
+# Deliverables: 10%
+* Your GitHub repository containing your application code.
+* Walkthrough Video: 27%
+* A walkthrough video that demonstrates the functionality of the Employee Tracker must be submitted, and a link to the video should be included in your README file.
+
+* The walkthrough video must show all of the technical acceptance criteria being met.
+
+* The walkthrough video must demonstrate how a user would invoke the application from the command line.
+
+* The walkthrough video must demonstrate a functional menu with the options outlined in the acceptance criteria.
+
+# Technical Acceptance Criteria: 40%
+* Satisfies all of the preceding acceptance criteria plus the following:
+
+* Uses the Inquirer package (Links to an external site.).
+
+* Uses the MySQL2 package (Links to an external site.) to connect to a MySQL database.
+
+* Uses the console.table package (Links to an external site.) to print MySQL rows to the console.
+
+* Follows the table schema outlined in the Challenge instructions.
+
+# Repository Quality: 13%
+* Repository has a unique name.
+
+* Repository follows best practices for file structure and naming conventions.
+
+* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
+
+* Repository contains multiple descriptive commit messages.
+
+* Repository contains a high-quality README with description and a link to a walkthrough video.
+
+# Application Quality 10%
+* The application user experience is intuitive and easy to navigate.
---
+++ server.js
@@ -0,0 +1,210 @@
+// DEPENDENCIES
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const consoleTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'sarah',
+    database: 'company'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+
+    console.log('Employee Tracker Iniatated');
+
+    menu();
+})
+
+// FUNCTIONS
+function menu() {
+    inquirer.prompt([{
+        name: 'menu',
+        type: 'list',
+        message: "Please select a command: ",
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit']
+    }])
+        .then(respsonse => {
+            switch (response.menu) {
+                case 'View all departments':
+                    viewDepartments();
+                    break;
+                case 'View all roles':
+                    viewRoles();
+                    break;
+                case 'View all employees':
+                    viewEmployees();
+                    break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Update an employee role':
+                    updateEmployee();
+                    break;
+                case 'Quit':
+                    connection.end();
+                    break;
+            }
+        })
+};
+
+// 'PREVIEW' FUNCTIONS
+function viewDepartments() {
+    connection.query('SELECT * FROM department', (error, result) => {
+        if (error) throw error;
+
+        console.table(result);
+
+        menu();
+    })
+};
+
+function viewRoles() {
+    connection.query('SELECT * FROM role', (error, result) => {
+        if (error) throw error;
+
+        console.table(result);
+
+        menu();
+    })
+};
+
+function viewEmployees() {
+    connection.query('SELECT * FROM employee', (error, result) => {
+        if (error) throw error;
+
+        console.table(result);
+
+        menu();
+    })
+};
+
+// 'ADD' FUNCTIONS
+function addDepartment() {
+    inquirer.prompt([{
+        name: 'name',
+        type: 'input',
+        message: 'Input the department name: '
+    }])
+        .then(response => {
+            connection.query('INSERT INTO department (name) VALUES (?)', [response.name], (error, result) => {
+                if (error) throw error;
+            })
+
+            viewDepartments();
+        })
+};
+
+function addRole() {
+    inquirer.prompt([{
+        name: 'name',
+        type: 'input',
+        message: 'Input the role title: '
+    },
+    {
+        name: 'salary',
+        type: 'number',
+        message: 'Input salary: ',
+        validate: salary => {
+            if (salary) {
+                return true;
+            } else {
+                console.log('Please enter a numerical value');
+                return false;
+            }
+        }
+    },
+    {
+        name: 'department',
+        type: 'list',
+        message: 'Select from departments:',
+        choices: getDepartments()
+    }
+    ])
+        .then(res => {
+            connection.query('INSERT INTO role SET ?', {
+                title: res.role,
+                salary: res.salary,
+                department_id: res.department
+            },
+
+                (err, res) => {
+                    if (err) throw err;
+                    console.log('Role has been added')
+
+                    menu();
+                })
+        })
+};
+
+function addEmployee() {
+    inquirer.prompt([{
+        name: 'firstName',
+        type: 'input',
+        message: 'Input first name of employee: '
+    },
+    {
+        name: 'lastName',
+        type: 'input',
+        message: 'Input last name of employee: '
+    },
+    {
+        name: 'role',
+        type: 'list',
+        message: 'Select role:',
+        choices: getRoles()
+    },
+    {
+        name: 'manager',
+        type: 'list',
+        message: 'Select reporting manager:',
+        choices: getManagers()
+    }
+    ])
+        .then(response => {
+
+            connection.query('INSERT INTO employee SET ?', {
+                first_name: response.firstName,
+                last_name: response.lastName,
+                role_id: response.role,
+                manager_id: response.manager
+            },
+
+                (err, res) => {
+                    if (err) throw err;
+                    console.log('Employee created.')
+
+                    menu();
+                })
+        })
+    };
+
+    function updateEmployee() {
+        inquirer.prompt([{
+                    name: 'employee',
+                    type: 'number',
+                    message: 'Enter employee ID'
+                },
+                {
+                    name: 'role',
+                    type: 'number',
+                    message: 'Enter role ID:'
+                }
+            ])
+            .then(response => {
+                connection.query('UPDATE employee SET role_id = ? WHERE id = ? ', [response.role, response.employee], (error, result) => {
+                    if (error) throw error;
+    
+                    viewEmployees();
+                })
+            })
+    };
